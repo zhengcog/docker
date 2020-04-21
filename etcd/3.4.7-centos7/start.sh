788c641f90ebc88f28db49aba2f5cbfe3bd11e9f
Hyman <zhengcog@gmail.com>
2020-04-21 22:25:43 +0800

    新增etcd

---
+++ etcd/3.4.7-centos7/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+mkdir /etcd-data
+/usr/local/bin/etcd \
+  --name s1 \
+  --data-dir /etcd-data \
+  --listen-client-urls http://0.0.0.0:2379 \
+  --advertise-client-urls http://0.0.0.0:2379 \
+  --listen-peer-urls http://0.0.0.0:2380 \
+  --initial-advertise-peer-urls http://0.0.0.0:2380 \
+  --initial-cluster s1=http://0.0.0.0:2380 \
+  --initial-cluster-token tkn \
+  --initial-cluster-state new \
+  --log-level info \
+  --logger zap \
+  --log-outputs stderr
